2f97b7b35d0f6c5aea3284d2e093077da1df7aa6
Mateusz Rosenkranz <mateusz.rosenkranz@apator.com>
2025-02-11 14:53:59 +0100

    Date: Tue Feb 11 14:53:36 2025 +0100 Signed-off-by: kedziorno <kjoblak85@gmail.com>

---
+++ IPv4.h
@@ -0,0 +1,29 @@
+#ifndef IPV4_H
+#define IPV4_H
+
+#include <stdint.h>
+
+void init(void);
+
+int add(unsigned int base, char mask);
+
+int del(unsigned int base, char mask);
+
+char check(unsigned int ip);
+
+void clear(void);
+
+// ghirda
+void __do_global_dtors_aux(void);
+void _fini(void);
+void _init(void);
+void frame_dummy(void);
+uint32_t checkIfPrefixExists(uint32_t param_1,char param_2);
+uint32_t checkIfPrefixIsValid(uint32_t param_1,char param_2);
+void print(void);
+void print_asIPV4(void);
+void runTests(void*);
+uint32_t size();
+
+#endif
+
---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "IPv4.h"
+
+int main (int argc, char *argv[], char *env[]) {
+init();
+runTests((void *)0); // function cleaning structure at end
+
+uint32_t uVar1;
+typedef unsigned long ulong;
+
+clear();
+init();
+print();
+uVar1 = add(0xaaaaaaaa,0x0);
+uVar1 = add(0xbbbbbbbb,0x0);
+uVar1 = size();
+printf("Size for python %u\n",(ulong)uVar1);
+print();
+print_asIPV4();
+
+}
+
