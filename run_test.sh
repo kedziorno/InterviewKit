d0e7c052aca1512ec76064b6f95e46c1c15ba8f8
kedziorno <kjoblak85@gmail.com>
2025-02-13 18:05:49 +0100

    run_test.sh Date: Thu Feb 13 17:46:46 2025 +0100
    run_test.sh
    Date: Thu Feb 13 18:05:49 2025 +0100
    
    Date: Thu Feb 13 18:05:49 2025 +0100

---
+++ run_test.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -x
+
+./run_testrunner.sh
+
+TESTRUNNER="pytest_c_testrunner"
+
+rm -rf build
+mkdir build
+cd build
+cmake ..
+make
+cp ../${TESTRUNNER}/conftest.py conftest.py.new
+sed -i 's/if path.ext == ".c" and path.basename.startswith("test_"):/if path.basename.startswith("InterviewKit"):/g' conftest.py.new
+grep "InterviewKit" conftest.py.new
+ln -s conftest.py.new conftest.py
+pytest .
+
