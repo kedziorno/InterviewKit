c676d8c8e3ca3cd39cf48f5c66f5009d8979ae82
kedziorno <kjoblak85@gmail.com>
2025-02-13 15:16:21 +0100

    newSet variable Date: Wed Feb 12 18:31:04 2025 +0100
    newSet variable
    Date: Thu Feb 13 15:16:21 2025 +0100
    
    newSet variable
    Date: Thu Feb 13 15:36:55 2025 +0100
    
    newSet variable
    Date: Thu Feb 13 15:37:46 2025 +0100
    
    Date: Thu Feb 13 15:16:21 2025 +0100

--- main.c
+++ main.c
@@ -1,22 +1,77 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <dlfcn.h>
+
+#include "test_macros_uint8.h"
+#include "test_macros_uint16.h"
+#include "test_macros_uint32.h"
+
 #include "IPv4.h"
 
 int main (int argc, char *argv[], char *env[]) {
+
+(void)argc;
+(void)argv;
+(void)env;
+
+void *handle;
+handle = dlopen ("../IPv4.so", RTLD_NOW | RTLD_GLOBAL);
+if (!handle) {
+  fprintf(stderr, "%s\n", dlerror());
+  exit(EXIT_FAILURE);
+}
+dlerror();
+
 init();
+
 runTests((void *)0); // function cleaning structure at end
 
-uint32_t uVar1;
-typedef unsigned long ulong;
-
 clear();
-init();
-print();
-uVar1 = add(0xaaaaaaaa,0x0);
-uVar1 = add(0xbbbbbbbb,0x0);
-print();
-print_asIPV4();
-uVar1 = size();
-printf("Size for python %u\n",(ulong)uVar1);
 
+init();
+
+print();
+
+uint32_t uVar1;
+
+uVar1 = add(0xaaaaaaaa,0x0);
+uVar1 = add(0x11111111,0x0);
+uVar1 = add(0x22222222,0x0);
+uVar1 = add(0x33333333,0x0);
+uVar1 = add(0x44444444,0x0);
+uVar1 = add(0x55555555,0x0);
+uVar1 = add(0x66666666,0x0);
+uVar1 = add(0x77777777,0x0);
+uVar1 = add(0x88888888,0x0);
+
+print();
+
+print_asIPV4();
+
+uVar1 = size();
+printf("Size %u\n", uVar1);
+
+uint64_t *p1 = NULL;
+p1 = (uint64_t *)dlsym(handle, "newSet"); // variable from IPv4.so
+printf ("newSet heap address = 0x%08lx\n", *p1);
+
+char *error = dlerror();
+if (error != NULL) {
+  fprintf(stderr, "aaaa %s\n", error);
+  exit(EXIT_FAILURE);
+}
+
+for (uint32_t i = 0;  i < uVar1; i++) {
+  uint32_t *q = (uint32_t*)*p1+(i*2)+0;
+  uint32_t *s = (uint32_t*)*p1+(i*2)+1;
+  printf ("%d(%x) : %08x,%08x\n", i, i, *q, *s);
+}
+
+printf ("\n");
+
+dlclose(handle);
+
+exit (EXIT_SUCCESS);
 }
 
--- run_testrunner.sh
+++ run_testrunner.sh
@@ -10,7 +10,7 @@ cd ${TESTRUNNER}
 rm -rf *
 git clean -fdx
 git reset --hard HEAD
-git apply ../${TESTRUNNER_PATH}
+git apply --reject ../${TESTRUNNER_PATH}
 make
-pytest
+pytest $@
 
